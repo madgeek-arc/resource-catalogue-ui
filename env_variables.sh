8f94b28c68b524e462e17124c2afecb45262c082
Konstantinos Spyrou <spyroukon@gmail.com>
2019-04-23 12:01:46 +0300

    crete temp file and replace original

--- env_variables.sh
+++ env_variables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-envsubst '${MATOMO_HOST} ${MATOMO_SITE_ID}' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html &&
+envsubst '${MATOMO_HOST} ${MATOMO_SITE_ID}' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp &&
+mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html &&
 envsubst '${PLATFORM_API_ENDPOINT}' < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf &&
 
 nginx -g "daemon off;"
