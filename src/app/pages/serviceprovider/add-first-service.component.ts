91e00d9f1de3a4bd9916a22157d566c7127400a5
jBalasis <cs2180008@di.uoa.gr>
2019-05-20 16:29:18 +0300

    updated add-first-service.component to properly display editFirstService page

--- src/app/pages/serviceprovider/add-first-service.component.ts
+++ src/app/pages/serviceprovider/add-first-service.component.ts
@@ -1,9 +1,11 @@
 import {Component, Injector, OnInit} from '@angular/core';
-import {AuthenticationService} from '../../services/authentication.service';
-import {Service} from '../../domain/eic-model';
-import {ServiceFormComponent} from '../eInfraServices/service-form.component';
 import {ActivatedRoute} from '@angular/router';
+import {DatePipe} from '@angular/common';
+import {ServiceFormComponent} from '../eInfraServices/service-form.component';
+import {AuthenticationService} from '../../services/authentication.service';
 import {ResourceService} from '../../services/resource.service';
+import {Service} from '../../domain/eic-model';
+import {MeasurementsPage} from "../../domain/indicators";
 
 @Component({
   selector: 'app-add-first-service',
@@ -16,7 +18,8 @@ export class AddFirstServiceComponent extends ServiceFormComponent implements On
 
   constructor(protected injector: Injector,
               protected authenticationService: AuthenticationService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private datePipe: DatePipe) {
     super(injector, authenticationService);
     this.editMode = false;
   }
@@ -33,10 +36,22 @@ export class AddFirstServiceComponent extends ServiceFormComponent implements On
 
         /*if (this.userService.canEditService(service)) {*/
         ResourceService.removeNulls(service);
-        this.serviceForm.patchValue(this.toForms(service));
+        this.formPrepare(service);
+        this.serviceForm.patchValue(service);
+        const lastUpdate = new Date(this.serviceForm.get('lastUpdate').value);
+        const date = this.datePipe.transform(lastUpdate, 'yyyy-MM-dd');
+        this.serviceForm.get('lastUpdate').setValue(date);
+        if (this.serviceForm.get('validFor').value) {
+          const validFor = new Date(this.serviceForm.get('validFor').value);
+          const validForDate = this.datePipe.transform(validFor, 'yyyy-MM-dd');
+          this.serviceForm.get('validFor').setValue(validForDate);
+        }
         /*} else {
             this.location.back();
         }*/
+      });
+      this.resourceService.getServiceMeasurements(this.serviceID).subscribe(measurements => {
+        this.measurementsFormPatch(measurements);
       });
     }
   }
@@ -45,26 +60,72 @@ export class AddFirstServiceComponent extends ServiceFormComponent implements On
     this.successMessage = 'Service uploaded successfully!';
   }
 
+  formPrepare(service: Service) {
+    for (let i = 0; i < service.providers.length - 1; i++) {
+      this.push('providers', true);
+    }
+    for (let i = 0; i < service.places.length - 1; i++) {
+      this.push('places', true);
+    }
+    for (let i = 0; i < service.languages.length - 1; i++) {
+      this.push('languages', true);
+    }
+    if (service.tags) {
+      for (let i = 0; i < service.tags.length - 1; i++) {
+        this.push('tags', false);
+      }
+    }
+    if (service.requiredServices) {
+      for (let i = 0; i < service.requiredServices.length - 1; i++) {
+        this.push('requiredServices', false);
+      }
+    }
+    if (service.relatedServices) {
+      for (let i = 0; i < service.relatedServices.length - 1; i++) {
+        this.push('relatedServices', false);
+      }
+    }
+    if (service.termsOfUse) {
+      for (let i = 0; i < service.termsOfUse.length - 1; i++) {
+        this.push('termsOfUse', false, true);
+      }
+    }
+  }
+
+  measurementsFormPatch(measurements: MeasurementsPage) {
+    for (let i = 0; i < measurements.results.length; i++) {
+      this.pushToMeasurements();
+      for (const j in measurements.results[i]) {
+        // console.log(j);
+        // console.log(measurements.results[i][j]);
+        if (measurements.results[i][j] !== null) {
+          this.measurements.controls[i].get(j).enable();
+          if (this.measurements.controls[i].get(j).value.constructor === Array) {
+            // console.log(this.measurements.controls[i].get(j).value);
+            for (let k = 0; k < measurements.results[i][j].length - 1; k++) {
+              this.pushToLocations(i);
+            }
+          }
+          if (j === 'valueIsRange') { // forms/html cooperate better with strings instead of boolean
+            this.measurements.controls[i].get(j).setValue(measurements.results[i][j] + '');
+          } else {
+            this.measurements.controls[i].get(j).setValue(measurements.results[i][j]);
+          }
+        } else {
+          this.measurements.controls[i].get(j).disable();
+        }
+      }
+      const time = new Date(this.measurements.controls[i].get('time').value);
+      const date = this.datePipe.transform(time, 'yyyy-MM-dd');
+      this.measurements.controls[i].get('time').setValue(date);
+    }
+    // console.log(measurements.results);
+  }
+
   onSubmit(service: Service, isValid: boolean) {
     if (this.serviceId) {
       service.id = this.serviceId;
     }
     super.onSubmit(service, isValid);
-  }
-
-  toForms(service: Service) {
-    const ret = {};
-    Object.entries(service).forEach(([name, values]) => {
-      let newValues = [];
-      if (Array.isArray(values)) {
-        values.forEach(entry => {
-          newValues.push({entry});
-        });
-      } else {
-        newValues = values;
-      }
-      ret[name] = newValues;
-    });
-    return <Service>ret;
   }
 }
