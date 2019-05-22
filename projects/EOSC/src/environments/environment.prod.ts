e7be4e500fc32960c7da0f9918a79006f7d79b19
Konstantinos Spyrou <spyroukon@gmail.com>
2019-05-22 15:25:41 +0300

    replaced previous change, added FAQ/API_ENDPOINT to eosc subproject environment files

--- projects/EOSC/src/environments/environment.prod.ts
+++ projects/EOSC/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  FAQ_ENDPOINT: 'http://beta.einfracentral.eu/faq/api',
+  API_ENDPOINT: '/api'
 };
--- projects/EOSC/src/environments/environment.ts
+++ projects/EOSC/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  FAQ_ENDPOINT: 'http://beta.einfracentral.eu/faq/api',
+  API_ENDPOINT: 'http://localhost:8080/eic-registry'
 };
 
 /*
