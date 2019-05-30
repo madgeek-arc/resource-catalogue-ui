6fa108608b01716254ca1ad137d60e376c05afa9
TheQuaker <cs2180008@di.uoa.gr>
2019-05-30 14:31:24 +0300

    minor fixes

--- projects/openaire-service-catalogue/src/app/pages/search/search.aire.component.ts
+++ projects/openaire-service-catalogue/src/app/pages/search/search.aire.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {SearchComponent} from '../../../../../../src/app/pages/search/search.component';
+import {Provider} from "../../../../../../src/app/domain/eic-model";
 
 declare var UIkit: any;
 
@@ -8,14 +9,24 @@ declare var UIkit: any;
   templateUrl: './search.aire.component.html',
   styleUrls: ['./search.aire.component.css']
 })
-export class SearchAireComponent extends SearchComponent implements OnInit{
+export class SearchAireComponent extends SearchComponent implements OnInit {
+  canAddOrEditService: boolean;
+  myProviders:  Provider[] = [];
 
   ngOnInit() {
-    this.pageSize = 9;
     super.ngOnInit();
+    this.canAddOrEditService = false;
+    this.pageSize = 9;
     this.listViewActive = false;
     // this 2 lines should be done better
+    this.resourceService.getMyServiceProviders().subscribe(
+      res => this.myProviders = res,
+      er => console.log(er),
+      () => this.canAddOrEditService = this.myProviders.some(p => p.id === 'openaire')
+    );
+
     this.updatePagingURLParametersQuantity(this.pageSize);
     this.navigateUsingParameters();
+
   }
 }
