a87891967368889e607b19a44bdf4daba1137d4e
TheQuaker <cs2180008@di.uoa.gr>
2019-07-10 14:17:34 +0300

    authentication interceptor

--- src/app/services/authentication-interceptor.ts
+++ src/app/services/authentication-interceptor.ts
@@ -1,34 +1,31 @@
-import {Injectable} from '@angular/core';
+import {Injectable, Injector} from '@angular/core';
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
 
 
 import {Router} from '@angular/router';
 import {Observable, throwError } from 'rxjs';
-import {map, catchError } from 'rxjs/operators';
+import {map, catchError, tap} from 'rxjs/operators';
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router) {
+  constructor(private injector: Injector,
+              private router: Router) {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     return next.handle(request).pipe(
-      map((event: HttpEvent<any>) => {
-        if (event instanceof HttpResponse) {
-          // do stuff with response if you want
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 403) {
+          this.router.navigate(['/forbidden']);
+        } else if (error.status === 404) {
+          this.router.navigate(['/notFound']);
         }
-        return event;
-      }, (err: any) => {
-        if (err instanceof HttpErrorResponse) {
-          console.log(err);
-          if (err.status === 403) {
-            // show forbidden page
-            console.log('Unauthorised!!', err);
-            this.router.navigateByUrl('/forbidden', {skipLocationChange: true});
-          }
-        }
-      }));
+        return throwError(error);
+      })
+    );
+
   }
+
 }
