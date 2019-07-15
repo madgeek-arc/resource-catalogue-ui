c7f3866843d5b2271e4c8e12b1b7e057855c9928
Konstantinos Spyrou <spyroukon@gmail.com>
2019-07-15 15:30:08 +0300

    updated eic-model

--- src/app/domain/eic-model.ts
+++ src/app/domain/eic-model.ts
@@ -1,144 +1,197 @@
 /* tslint:disable */
-// Generated using typescript-generator version 2.6.433 on 2019-02-18 16:03:02.
-
-export class Event implements Identifiable {
-    id: string;
-    instant: number;
-    type: string;
-    user: string;
-    service: string;
-    value: string;
-}
-
-export class Funder implements Identifiable {
-    id: string;
-    name: string;
-    logo: URL;
-    services: string[];
-}
+// Generated using typescript-generator version 2.6.433 on 2019-07-15 15:25:21.
 
 export interface Identifiable {
     id: string;
 }
 
+export class Event implements Identifiable {
+  id: string;
+  instant: number;
+  type: string;
+  user: string;
+  service: string;
+  value: string;
+}
+
+export class ExtrasMapType {
+  entry: ExtrasType[];
+}
+
+export class XmlAdapter<ValueType, BoundType> {
+}
+
+export class ExtrasMapTypeAdapter extends XmlAdapter<ExtrasMapType, { [index: string]: string }> {
+}
+
+export class ExtrasType {
+  key: string;
+  value: string;
+}
+
+export class Funder implements Identifiable {
+  id: string;
+  name: string;
+  logo: URL;
+  services: string[];
+}
+
+export interface Identifiable {
+  id: string;
+}
+
 export class Indicator implements Identifiable {
-    id: string;
-    name: string;
-    description: string;
-    dimensions: string[];
-    unit: string;
-    unitName: string;
+  id: string;
+  name: string;
+  description: string;
+  dimensions: string[];
+  unit: string;
+  unitName: string;
 }
 
 export class Service implements Identifiable {
-    id: string;
-    url: URL;
-    name: string;
-    tagline: string;
-    description: string;
-    options: string;
-    targetUsers: string;
-    userValue: string;
-    userBase: string;
-    symbol: URL;
-    multimediaURL: URL;
-    providers: string[];
-    version: string;
-    lastUpdate: XMLGregorianCalendar;
-    changeLog: string;
-    validFor: XMLGregorianCalendar;
-    lifeCycleStatus: string;
-    trl: string;
-    category: string;
-    subcategory: string;
-    places: string[];
-    languages: string[];
-    tags: string[];
-    requiredServices: string[];
-    relatedServices: string[];
-    order: URL;
-    helpdesk: URL;
-    userManual: URL;
-    trainingInformation: URL;
-    feedback: URL;
-    price: URL;
-    serviceLevelAgreement: URL;
-    termsOfUse: string[];
-    funding: string;
+  id: string;
+  name: string;
+  url: URL;
+  description: string;
+  logo: URL;
+  tagline: string;
+  userValue: string;
+  userBases: string[];
+  useCases: string[];
+  multimediaUrls: URL[];
+  options: string[];
+  requiredServices: string[];
+  relatedServices: string[];
+  providers: string[];
+  scientificDomains: string[];
+  scientificSubdomains: string[];
+  category: string;
+  subcategories: string[];
+  supercategory: string;
+  targetUsers: string[];
+  languages: string[];
+  places: string[];
+  accessTypes: string[];
+  accessModes: string[];
+  funders: string[];
+  tags: string[];
+  phase: string;
+  trl: string;
+  version: string;
+  lastUpdate: XMLGregorianCalendar;
+  changeLog: string;
+  certifications: string[];
+  standards: string[];
+  orderType: string;
+  order: URL;
+  sla: URL;
+  termsOfUse: URL;
+  privacyPolicy: URL;
+  accessPolicy: URL;
+  paymentModel: URL;
+  pricing: URL;
+  manual: URL;
+  training: URL;
+  helpdesk: URL;
+  monitoring: URL;
+  maintenance: URL;
+  ownerName: string;
+  ownerContact: string;
+  supportName: string;
+  supportContact: string;
+  securityName: string;
+  securityContact: string;
 }
 
 export class InfraService extends Service {
-    serviceMetadata: ServiceMetadata;
-    active: boolean;
-    status: string;
-    latest: boolean;
-}
-
-export class RangeValue {
-    fromValue: string;
-    toValue: string;
+  serviceMetadata: ServiceMetadata;
+  active: boolean;
+  status: string;
+  latest: boolean;
 }
 
 export class Measurement implements Identifiable {
-    id: string;
-    indicatorId: string;
-    serviceId: string;
-    time: XMLGregorianCalendar;
-    locations: string[];
-    valueIsRange: boolean;
-    value: string;
-    rangeValue: RangeValue;
+  id: string;
+  indicatorId: string;
+  serviceId: string;
+  time: XMLGregorianCalendar;
+  locations: string[];
+  valueIsRange: boolean;
+  value: string;
+  rangeValue: RangeValue;
 }
 
 export class Provider implements Identifiable {
-    id: string;
-    name: string;
-    website: URL;
-    catalogueOfResources: URL;
-    publicDescOfResources: URL;
-    logo: URL;
-    additionalInfo: string;
-    contactInformation: string;
-    users: User[];
-    active: boolean;
-    status: string;
+  id: string;
+  name: string;
+  website: URL;
+  description: string;
+  logo: URL;
+  contactName: string;
+  contactEmail: string;
+  contactTel: string;
+  active: boolean;
+  status: string;
+  users: User[];
 }
 
-export class RichService extends Service {
-    categoryName: string;
-    subCategoryName: string;
-    trlName: string;
-    lifeCycleStatusName: string;
-    languageNames: string[];
-    placeNames: string[];
-    views: number;
-    ratings: number;
-    userRate: number;
-    hasRate: number;
-    favourites: number;
-    isFavourite: boolean;
+export class RangeValue {
+  fromValue: string;
+  toValue: string;
+}
+
+export class RichService {
+  service: Service;
+  serviceMetadata: ServiceMetadata;
+  superCategoryName: string;
+  categoryName: string;
+  subCategoryNames: string[];
+  languageNames: string[];
+  placeNames: string[];
+  trlName: string;
+  phaseName: string;
+  scientificDomainNames: string[];
+  scientificSubDomainNames: string[];
+  targetUsersNames: string[];
+  accessTypeNames: string[];
+  accessModeNames: string[];
+  fundedByNames: string[];
+  orderTypeName: string;
+  views: number;
+  ratings: number;
+  userRate: number;
+  hasRate: number;
+  favourites: number;
+  isFavourite: boolean;
 }
 
 export class ServiceMetadata {
-    performanceData: Measurement[];
-    featured: boolean;
-    published: boolean;
-    registeredBy: string;
-    registeredAt: string;
-    modifiedBy: string;
-    modifiedAt: string;
+  registeredBy: string;
+  registeredAt: string;
+  modifiedBy: string;
+  modifiedAt: string;
 }
 
 export class ServiceHistory extends ServiceMetadata {
-    version: string;
-    versionChange: boolean;
+  version: string;
+  versionChange: boolean;
+  coreVersionId: string;
+}
+
+export class ServiceOption implements Identifiable {
+  id: string;
+  name: string;
+  url: URL;
+  description: string;
+  logo: URL;
 }
 
 export class User implements Identifiable {
-    id: string;
-    email: string;
-    name: string;
-    surname: string;
+  id: string;
+  email: string;
+  name: string;
+  surname: string;
 }
 
 export class Vocabulary implements Identifiable {
@@ -173,17 +226,8 @@ export class URL implements Serializable {
 export class XMLGregorianCalendar implements Cloneable {
 }
 
-export class ExtrasMap {
-    entries: Extras[];
-}
-
 export interface Serializable {
 }
 
 export interface Cloneable {
-}
-
-export class Extras {
-    key: string;
-    value: string;
 }
