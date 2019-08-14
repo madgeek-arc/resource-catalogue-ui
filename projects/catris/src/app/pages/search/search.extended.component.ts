2fcf144a40108913deb2bf34db2056c910853d11
jBalasis <cs2180008@di.uoa.gr>
2019-08-14 18:46:28 +0300

    email checkbox and email modal added to catris search page

--- projects/catris/src/app/pages/search/search.extended.component.ts
+++ projects/catris/src/app/pages/search/search.extended.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {SearchComponent} from '../../../../../../src/app/pages/search/search.component';
+import {FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -8,4 +9,29 @@ import {SearchComponent} from '../../../../../../src/app/pages/search/search.com
   styleUrls: ['./search.component.css']
 })
 export class SearchExtendedComponent extends SearchComponent implements OnInit, OnDestroy {
+  public serviceIdsArray: string[] = [];
+  emailForm = this.fb.group({
+    email: ['', Validators.compose([Validators.required, Validators.email])],
+    subject: ['', Validators.required],
+    text: ['', Validators.required],
+  });
+
+  isChecked(serviceId: string) {
+    return this.serviceIdsArray.indexOf(serviceId) > -1;
+  }
+
+  addOrRemove(serviceId: string) {
+    const pos = this.serviceIdsArray.indexOf(serviceId);
+    if (pos > -1) {
+      this.serviceIdsArray.splice(pos, 1);
+    } else {
+      this.serviceIdsArray.push(serviceId);
+    }
+    console.log(this.serviceIdsArray);
+  }
+
+  resetForm() {
+    this.emailForm.reset('');
+  }
+
 }
