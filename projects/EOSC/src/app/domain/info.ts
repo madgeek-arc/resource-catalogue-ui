f01799a3fccc69e50488f09b0cacf5804a0c7167
Konstantinos Spyrou <spyroukon@gmail.com>
2019-09-12 15:26:52 +0300

    changed homepage info about services and providers

--- projects/EOSC/src/app/domain/info.ts
+++ projects/EOSC/src/app/domain/info.ts
@@ -2,11 +2,12 @@
  * Created by stefania on 22/11/18.
  */
 export class Info {
-    language: number;
-    services: number;
-    category: number;
-    subcategory: number;
-    providers: number;
-    lifeCycleStatus: number;
-    trl: number;
+  language: string;
+  services: string;
+  serviceEntries: string;
+  category: string;
+  subcategory: string;
+  providers: string;
+  lifeCycleStatus: string;
+  trl: string;
 }
