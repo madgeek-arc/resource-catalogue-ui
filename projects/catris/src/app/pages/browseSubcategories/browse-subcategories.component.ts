874997d600671eb478e4cd07533d57c6ae41c129
TheQuaker <cs2180008@di.uoa.gr>
2019-09-17 13:06:29 +0300

    browse subgategories now are clickable

--- projects/catris/src/app/pages/browseSubcategories/browse-subcategories.component.ts
+++ projects/catris/src/app/pages/browseSubcategories/browse-subcategories.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ResourceService} from '../../../../../../src/app/services/resource.service';
 import {NestedVocabulary} from '../../../../../../src/app/domain/eic-model';
+import {NavigationService} from '../../../../../../src/app/services/navigation.service';
 
 @Component({
   selector: 'app-browse-subcategories',
@@ -10,11 +11,15 @@ export class BrowseSubcategoriesComponent implements OnInit {
   public errorMessage: string;
   public nestedStructure: NestedVocabulary;
 
-  constructor(private resourceService: ResourceService) {}
+  constructor(private resourceService: ResourceService,
+              public router: NavigationService) {}
 
   ngOnInit(): void {
     this.resourceService.getNestedVocabulariesByType('SUPERCATEGORY').subscribe(
-      res => this.nestedStructure = res,
+      res => {
+        this.nestedStructure = res;
+        this.nestedStructure.children.sort((a, b) => 0 - (a.vocabulary.name > b.vocabulary.name ? -1 : 1));
+      },
       error => console.log(error)
     );
   }
