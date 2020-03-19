0524258d0101b00a9bdc0bcf0238729a214ad827
Konstantinos Spyrou <spyroukon@gmail.com>
2020-03-20 01:42:55 +0200

    added email and telephone validations in service form

--- src/app/pages/eInfraServices/service-form.component.ts
+++ src/app/pages/eInfraServices/service-form.component.ts
@@ -187,8 +187,8 @@ export class ServiceFormComponent implements OnInit {
       this.fb.group({
         firstName: ['', Validators.required],
         lastName: ['', Validators.required],
-        email: ['', Validators.required],
-        tel: ['', Validators.required],
+        email: ['', Validators.compose([Validators.required, Validators.email])],
+        tel: ['', Validators.compose([Validators.required, Validators.pattern('[+]?\\d+$')])],
         position: [''],
       }, Validators.required)
     ]),
