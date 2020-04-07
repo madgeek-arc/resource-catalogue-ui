e5ad16bebc0f8934b6911f5eba8c7dc369425aad
Stefania Martziou <stefania.martziou@imis.athena-innovation.gr>
2020-04-07 23:28:40 +0300

    Added the SP registration instructions pdf in a menu item under support

--- projects/catris/src/app/pages/support/privacypolicy/privacy-policy.component.ts
+++ projects/catris/src/app/pages/support/privacypolicy/privacy-policy.component.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-privacy-policy-catris',
   templateUrl: './privacy-policy.component.html',
-  // styleUrls: ['./developers.component.css']
 })
+
 export class PrivacyPolicyComponent {
 }
