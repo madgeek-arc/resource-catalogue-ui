e49ea1d467a4d853f2a4969d3f526daba42b3242
Konstantinos Spyrou <spyroukon@gmail.com>
2020-06-18 22:57:12 +0300

    replaced service landing pages of subprojects with page from parent project

--- projects/openaire-service-catalogue/src/app/pages/landingpages/service/service-landing-page-extended.component.ts
+++ projects/openaire-service-catalogue/src/app/pages/landingpages/service/service-landing-page-extended.component.ts
@@ -3,7 +3,7 @@ import {ServiceLandingPageComponent} from '../../../../../../../src/app/pages/la
 
 @Component({
   selector: 'app-landing-page',
-  templateUrl: 'service-landing-page.component.html',
+  templateUrl: './../../../../../../../src/app/pages/landingpages/service/service-landing-page.component.html',
   styleUrls: ['../landing-page.component.css']
 })
 
