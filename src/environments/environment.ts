c964e975b8152234e6a538de7c07dd6ac8af97a1
amantas <andreas_mantas@hotmail.com>
2020-09-21 12:51:19 +0300

    changed projectName variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  projectName: '{Project Name Goes Here}',
+  projectName: 'EOSC',
   FAQ_ENDPOINT: 'https://dl105.madgik.di.uoa.gr/faq/api',
   API_ENDPOINT: '/eic-registry', // to change the end point go to proxy.conf.json file
   hasUserConsent: true
