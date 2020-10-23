767a4ff802f46b3e07ceac5b6527f5169f7bc73f
Stefania Martziou <stefania.martziou@imis.athena-innovation.gr>
2020-10-23 13:47:53 +0300

    Created a beta environment properties file

---
+++ src/environments/environment.beta.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  beta: true,
+  FAQ_ENDPOINT: '/faq/api',
+  API_ENDPOINT: '/api',
+  projectName: 'EOSC',
+  projectMail: 'onboarding@eosc-portal.eu',
+  serviceORresource: 'Resource',
+  hasUserConsent: true,
+  privacyPolicyURL: 'https://www.eosc-portal.eu/privacy-policy-summary',
+  marketplaceBaseURL: 'https://marketplace-beta.docker-fid.grid.cyf-kr.edu.pl/services/',
+  API_TOKEN_ENDPOINT: 'https://aai.eosc-portal.eu/providers-api'
+};
