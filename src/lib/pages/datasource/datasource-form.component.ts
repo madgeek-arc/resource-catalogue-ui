028c673d9bac35fd692519aa1093735848a4bae0
amantas <andreas_mantas@hotmail.com>
2022-06-20 11:57:01 +0300

    change vocabulary for geographic location to have only countries

--- src/lib/pages/datasource/datasource-form.component.ts
+++ src/lib/pages/datasource/datasource-form.component.ts
@@ -478,7 +478,7 @@ export class DatasourceFormComponent implements OnInit {
         this.fundingBodyVocabulary = this.vocabularies[Type.FUNDING_BODY];
         this.fundingProgramVocabulary = this.vocabularies[Type.FUNDING_PROGRAM];
         this.relatedPlatformsVocabulary = this.vocabularies[Type.RELATED_PLATFORM];
-        // this.placesVocabulary = this.vocabularies[Type.COUNTRY];
+        this.placesVocabulary = this.vocabularies[Type.COUNTRY];
         this.geographicalVocabulary = Object.assign(this.vocabularies[Type.COUNTRY],this.vocabularies[Type.REGION]);
         this.languagesVocabulary = this.vocabularies[Type.LANGUAGE];
         // this.placesVocIdArray = this.placesVocabulary.map(entry => entry.id);
