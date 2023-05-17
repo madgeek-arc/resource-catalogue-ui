b71491b19aadaed2120297bcf6ac5571f2970b88
amantas <andreas_mantas@hotmail.com>
2023-05-17 18:00:03 +0300

    notes on some methods

--- src/lib/services/resource.service.ts
+++ src/lib/services/resource.service.ts
@@ -126,8 +126,7 @@ export class ResourceService {
   }
 
   getAllRelatedResources(){ // Gets services, datasources and trainings
-    return this.http.get(this.base + '/service/resourceIdToNameMap/'); // TODO: rename as bellow on backend redeploy
-    // return this.http.get(this.base + '/service/getAllProviderRelatedResources/');
+    return this.http.get(this.base + '/service/resourceIdToNameMap/');
   }
 
   getService(serviceId: string, catalogueId?: string) {
@@ -368,7 +367,7 @@ export class ResourceService {
     let params = new HttpParams();
     params = params.append('from', '0');
     params = params.append('quantity', '10000');
-    if (status === 'approved provider') {
+    if (status === 'approved provider') { //not matched hence never reached, do we need approved providers or all?
       return this.http.get<Paging<Provider>>(this.base + `/provider/all/?status=approved provider`, {params, withCredentials: true});
     }
     return this.http.get<Paging<Provider>>(this.base + `/provider/all/`, {params, withCredentials: true});
