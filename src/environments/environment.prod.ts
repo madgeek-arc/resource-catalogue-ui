30dfde4123d8bdd927176877dd8cf3c7341f7768
Konstantinos Spyrou <spyroukon@gmail.com>
2024-12-03 11:20:18 +0200

    Update environment.prod.ts
    fixed property

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   MATOMO_SITE: 1,
   FAQ_ENDPOINT: '/faq/api',
   API_ENDPOINT: '/api',
-  API_LOGIN: '/api/oauth2/authorization/google',
+  API_LOGIN: '/api/oauth2/authorization/eosc',
   STATS_ENDPOINT: 'https://providers.eosc-portal.eu/stats-api/',
   projectName: 'EOSC',
   projectMail: 'onboarding@eosc-portal.eu',
