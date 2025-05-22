2e8141facbbcf1ce66eae204deac5306d7d234eb
mzouros <michael.zouros@outlook.com>
2025-05-22 12:05:43 +0300

    fix: remove slash

--- src/lib/services/resource.service.ts
+++ src/lib/services/resource.service.ts
@@ -144,7 +144,7 @@ export class ResourceService {
 
   getMultipleResourcesById(commaSeparatedIds: string) { //feed with public ids (or not) of services, datasources, and training resources; returns only if resource exist; NOT bundles
     commaSeparatedIds = decodeURIComponent(commaSeparatedIds);
-    return this.http.get<any[]>(this.base + `/service/ids?ids=${commaSeparatedIds}/`, this.options);
+    return this.http.get<any[]>(this.base + `/service/ids?ids=${commaSeparatedIds}`, this.options);
   }
 
 /*  getServicesOfferedByProvider(id: string): Observable<RichService[]> {
