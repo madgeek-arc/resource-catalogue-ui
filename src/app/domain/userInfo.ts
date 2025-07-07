09fdf693b94a23338b9e246338177c16dcc53dbd
amantas <andreas_mantas@hotmail.com>
2025-07-07 12:48:00 +0300

    chore: code cleanup

--- src/app/domain/userInfo.ts
+++ src/app/domain/userInfo.ts
@@ -1,69 +1,6 @@
 export class UserInfo {
-  stakeholders: Stakeholder[];
-  coordinators: Coordinator[];
-  administrators: Administrator[];
-  user: User;
-  admin: boolean;
-
   constructor() {
-    this.user = new User();
   }
-}
-
-export class UserGroup {
-  id: string;
-  name: string;
-  type: string;
-  members: string[];
-  admins: string[];
-
-  constructor() {
-    this.name = null;
-    this.type = null;
-  }
-}
-
-export class Stakeholder extends UserGroup {
-  associationMember: string;
-  country: string;
-  subType: string;
-  mandated: boolean;
-
-  constructor() {
-    super();
-    this.id = null;
-    this.associationMember = null;
-    this.mandated = false;
-    this.country = null;
-    this.subType = null;
-  }
-
-}
-
-export class Coordinator extends UserGroup{}
-
-export class Administrator extends UserGroup{}
-
-
-export class User {
-  sub: string;
-  email: string;
-  name: string;
-  surname: string;
-  fullname: string;
-  policiesAccepted: PolicyAccepted[];
-  profile: Profile;
-  id: string
-
-  constructor() {
-    this.profile = new Profile();
-  }
-}
-
-export class PolicyAccepted {
-  id: string;
-  time: number;
-  acceptedDate: number;
 }
 
 export class Profile {
@@ -78,11 +15,6 @@ export class Profile {
     this.affiliation = null
     this.webpage = null
   }
-}
-
-export class GroupMembers {
-  members: User[];
-  admins: User[];
 }
 
 export class UserActivity {
--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -25,9 +25,6 @@ interface Action {
   providedIn: 'root'  // Ensures the service is available globally
 })
 export class WebsocketService {
-  private surveyAnswerId: string | null = null;
-  private type: string | null = null;
-  private dropConnection = false;
   private userSessionId: string | null = null;
 
   stompClient: Promise<typeof Stomp>;
@@ -39,78 +36,21 @@ export class WebsocketService {
   constructor() {}
 
   initializeWebSocketConnection(id: string, resourceType: string) {
-    const ws = new SockJS(URL);
-    const that = this;
-
-    this.dropConnection = false;
-    this.surveyAnswerId = id;
-    this.type = resourceType;
-
-    this.stompClient = new Promise((resolve, reject) => {
-      let stomp = Stomp.over(ws);
-
-      stomp.debug = null;
-      stomp.connect({}, function(frame) {
-        const timer = setInterval(() => {
-          if (stomp.connected) {
-            clearInterval(timer);
-            that.count = 0;
-            stomp.subscribe(`/topic/active-users/${that.type}/${that.surveyAnswerId}`, (message) => {
-              if (message.body) {
-                // console.log(message.headers['message-id']);
-                that.userSessionId = message.headers['message-id'].split('-')[0];
-                that.activeUsers.next(JSON.parse(message.body));
-                // console.log(that.activeUsers);
-              }
-            });
-            stomp.subscribe(`/topic/edit/${resourceType}/${that.surveyAnswerId}`, (message) => {
-              if (message.body) {
-                console.log('edit event, with body: ' + message.body);
-                that.edit.next(JSON.parse(message.body));
-                // console.log(that.edit);
-              }
-            });
-            resolve(stomp);
-          }
-        }, 1000);
-      }, function (error) {
-        let timeout = 1000;
-        that.count > 20 ? timeout = 10000 : that.count++ ;
-        setTimeout( () => {
-          // stomp.close();
-          that.initializeWebSocketConnection(that.surveyAnswerId, that.type)
-        }, timeout);
-        console.log('STOMP: Reconnecting...'+ that.count);
-      });
-    });
-
-    this.stompClient.then(client => client.ws.onclose = (event) => {
-      this.activeUsers.next(null);
-      if (!this.dropConnection)
-        this.initializeWebSocketConnection(that.surveyAnswerId, that.type);
-    });
   };
 
   WsLeave(action: string) { // {} is for headers
-    this.stompClient.then( client => client.send(`/app/leave/${this.type}/${this.surveyAnswerId}`, {}, action));
   }
 
   WsJoin(action: string) {
-    this.stompClient.then( client => client.send(`/app/join/${this.type}/${this.surveyAnswerId}`, {}, action));
   }
 
   WsFocus(field?: string, value?: string) {
-    this.stompClient.then( client => client.send(`/app/focus/${this.type}/${this.surveyAnswerId}/${field}`, {}, value));
   }
 
   WsEdit(value: { field: string; value: any; action?: Action; }) {
-    console.log(value);
-    this.stompClient.then( client => client.send(`/app/edit/${this.type}/${this.surveyAnswerId}`, {}, JSON.stringify(value)));
   }
 
   closeWs() {
-    this.dropConnection = true;
-    this.stompClient?.then(client => client.ws.close());
   }
 
   get userId() {
