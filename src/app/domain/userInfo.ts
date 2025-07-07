6fce27f0f13efe94bdd52ec6e272f7cc3a4b5942
amantas <andreas_mantas@hotmail.com>
2025-07-07 12:56:11 +0300

    chore: cleanup userInfo.ts

--- src/app/domain/userInfo.ts
+++ src/app/domain/userInfo.ts
@@ -1,22 +1,3 @@
-export class UserInfo {
-  constructor() {
-  }
-}
-
-export class Profile {
-  picture: string | ArrayBuffer;
-  position: string;
-  affiliation: string;
-  webpage: string;
-
-  constructor() {
-    this.picture = null
-    this.position = null
-    this.affiliation = null
-    this.webpage = null
-  }
-}
-
 export class UserActivity {
   sessionId: string;
   fullname: string;
