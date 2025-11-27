d0d0803cf09f84ab426d9a16e008824b1c96088f
amantas <48762391+amantas@users.noreply.github.com>
2025-11-27 13:35:44 +0200

    chore: Remove isLoginOrRegister conditional check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {NavigationEnd, Router} from '@angular/router';
-import {environment} from '../environments/environment';
 
 @Component({
     selector: 'app-root',
@@ -9,7 +8,6 @@ import {environment} from '../environments/environment';
     standalone: false
 })
 export class AppComponent implements OnInit {
-  isLoginOrRegister = false;
 
   breadcrumbs: string[] = [];
 
@@ -22,8 +20,6 @@ export class AppComponent implements OnInit {
         this.breadcrumbs = evt.url.split(/\//);
       }
       this.breadcrumbs[0] = 'Home';
-
-      // this.isLoginOrRegister = ["/signUp", "/signIn"].indexOf(evt.url) >= 0;
     });
 
     this.router.events.subscribe((evt) => {
